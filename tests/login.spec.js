9a07d7129a8d99a2e4ea774d88913e1409762f01
Gustavo Henrique <gustavohenrique@pactosolucoes.com.br>
2025-06-02 06:38:24 -0300

    feat: cria primeiro teste para login inválido

---
+++ tests/login.spec.js
@@ -0,0 +1,51 @@
+// @ts-check
+import { test, expect } from '@playwright/test';
+import { on } from 'events';
+
+test('Não deve logar quando o código de autenticacao for inválido', async ({ page }) => {
+
+  const usuario = {
+    cpf: '00000014141',
+    senha: '147258',
+  }
+
+  await page.goto('http://paybank-mf-auth:3000/');
+
+  await page.getByRole('textbox', { name: 'Digite seu CPF' }).fill('00000014141');
+  await page.getByRole('button', { name: 'Continuar' }).click();
+
+  for (const digito of usuario.senha) {
+    await page.getByRole('button', { name: digito }).click();
+  }
+
+  await page.getByRole('button', { name: 'Continuar' }).click();
+
+  await page.getByRole('textbox', { name: '000000' }).fill('123456');
+  await page.getByRole('button', { name: 'Verificar' }).click();
+
+  await expect(page.locator('span')).toContainText('Código inválido. Por favor, tente novamente.');
+});
+
+test('Deve acessar a conta do usuário', async ({ page }) => {
+
+  const usuario = {
+    cpf: '00000014141',
+    senha: '147258',
+  }
+
+  await page.goto('http://paybank-mf-auth:3000/');
+
+  await page.getByRole('textbox', { name: 'Digite seu CPF' }).fill('00000014141');
+  await page.getByRole('button', { name: 'Continuar' }).click();
+
+  for (const digito of usuario.senha) {
+    await page.getByRole('button', { name: digito }).click();
+  }
+
+  await page.getByRole('button', { name: 'Continuar' }).click();
+
+  await page.getByRole('textbox', { name: '000000' }).fill('123456');
+  await page.getByRole('button', { name: 'Verificar' }).click();
+
+  await expect(page.locator('span')).toContainText('Código inválido. Por favor, tente novamente.');
+});
